f82381c84b038fbbb0dd46658f7ce3916f706d6e
worawut aunkham <jameskater350@gmail.com>
2023-01-12 15:44:18 +0700

    Done of authentation

--- routes/auth_bp.py
+++ routes/auth_bp.py
@@ -1,8 +1,6 @@
 from flask import Blueprint
-
 from controller.authController import AuthController
 
-
 class AuthBlueprint:
-    auth_bp = Blueprint('auth_bp', __name__, url_prefix='/auth')
+    auth_bp = Blueprint('auth_bp', __name__, url_prefix='/login')
     auth_bp.route('/', methods=['POST'])(AuthController.auth)
